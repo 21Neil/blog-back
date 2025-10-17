b77492878e5fc54e538b240cd238e2b54735aa48
21Neil <88711171+21Neil@users.noreply.github.com>
2025-10-17 17:07:10 +0800

    Revert "feat: change error page"
    This reverts commit 7598f7ae23ab4682371ab0bad99ccec22a863c06.

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,14 +1,5 @@
-import style from './Error.module.css';
-
 export const Error = () => {
   return (
-    <main className={style.error}>
-      <h1 className={style.error__title}>404 Not found</h1>
-      <div className='btn-container'>
-        <a href='/dashboard'>
-          <button className='btn'>Back dashboard</button>
-        </a>
-      </div>
-    </main>
-  );
-};
+    <div>Error</div>
+  )
+}
