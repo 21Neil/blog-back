6ed6adcc08b53f01bc2ac8e2748dad71eb677b3c
21Neil <88711171+21Neil@users.noreply.github.com>
2025-10-17 17:18:38 +0800

    Revert "feat: apply chakra ui to login page"
    This reverts commit b293e1636c7824b705e629595fa6c6b4c403071f.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,12 @@
 import { useContext, useState } from 'react';
 import useFetch from '../../hooks/useFetch';
 import { AuthContext } from '../../context/AuthContext';
-import { Heading, VStack } from '@chakra-ui/react';
-import { InputField } from '../../components/InputField/InputField';
-import { useForm } from 'react-hook-form';
+import style from './Login.module.css';
 
 export const Login = () => {
   const [formdata, setFormdata] = useState(null);
   const { post } = useFetch();
   const { checkAuth } = useContext(AuthContext);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
 
   const submitOnClick = async e => {
     e.preventDefault();
@@ -21,46 +14,43 @@ export const Login = () => {
     await checkAuth();
   };
 
-  const onSubmit = handleSubmit(data => console.log(data));
-
   return (
-    <VStack
-      as='main'
-      justify='center'
-      minH='100vh'
-      transform='translateY(-7.5vh)'
-    >
-      <Heading
-        as='h1'
-        fontFamily='caveat'
-        fontSize='2.57rem'
-        fontWeight='normal'
-      >
-        Neil.
-      </Heading>
-      <VStack as='form' align='end' gap='5' onSubmit={onSubmit}>
-        <VStack gap='3'>
-          <InputField
-            label='Username'
-            type='text'
-            {...register('username', { required: 'Required' })}
-            error={errors.username?.message}
-            required
-          />
-          <InputField
-            label='Password'
-            type='password'
-            {...register('password')}
-            required
-            error={errors.password?.message}
-          />
-        </VStack>
-        <div className='btn-container'>
-          <button type='submit' className='btn'>
-            submit
-          </button>
+    <main className={style.login}>
+      <h1 className={style.login__title}>Neil.</h1>
+      <form className={style.login__form}>
+        <div className={style.login__inputGroup}>
+          <div className={style.login__field}>
+            <label htmlFor='username'>
+              Username
+            </label>
+            <input
+              type='text'
+              name='username'
+              id='username'
+              autoComplete='username'
+              onChange={e =>
+                setFormdata(prev => ({ ...prev, username: e.target.value }))
+              }
+            />
+          </div>
+          <div className={style.login__field}>
+            <label htmlFor='password'>
+              Password
+            </label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              onChange={e =>
+                setFormdata(prev => ({ ...prev, password: e.target.value }))
+              }
+            />
+          </div>
         </div>
-      </VStack>
-    </VStack>
+        <div className='btn-container'>
+          <button className='btn' onClick={submitOnClick}>submit</button>
+        </div>
+      </form>
+    </main>
   );
 };
