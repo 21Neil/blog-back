7beea9936c63cf6f480c0a47c245a439ee60456e
21Neil <88711171+21Neil@users.noreply.github.com>
2026-01-22 16:27:02 +0800

    feat: add compress image error handler

--- src/utils/compressImage.js
+++ src/utils/compressImage.js
@@ -1,13 +1,21 @@
 import imageCompression from 'browser-image-compression';
 
 const compressImage = async file => {
-  return await imageCompression(file, {
-    maxSizeMB: 1,
-    maxWidthOrHeight: 1440,
-    fileType: 'image/jpeg',
-    initialQuality: 0.9,
-    useWebWorker: true,
-  });
+  try {
+    const compressedImage = await imageCompression(file, {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1440,
+      fileType: 'image/jpeg',
+      initialQuality: 0.9,
+      useWebWorker: true,
+    });
+    
+    return compressedImage;
+  } catch (err) {
+    console.error('Image compress fail', err);
+
+    return file;
+  }
 };
 
 export default compressImage;
