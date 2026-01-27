e44cd42d924e80d2438339146d702f09709fe173
21Neil <88711171+21Neil@users.noreply.github.com>
2026-01-27 15:50:45 +0800

    fix: remove upload cover image blank button

--- src/components/ImageUploader/ImageUploader.jsx
+++ src/components/ImageUploader/ImageUploader.jsx
@@ -10,7 +10,7 @@ const ImageUploader = ({ form }) => (
         <p>Check to upload photo...</p>
       )}
       <FileInput
-        classNames={{ root: styles.imageInput }}
+        className={styles.imageInput}
         accept='image/*'
         key={form.key('image')}
         {...form.getInputProps('image')}
